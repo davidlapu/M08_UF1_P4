69324da67ed93798be0b29ff33306c419da8a3ff
David Lama Pulido <david.lama.7e3@itb.cat>
2020-11-04 18:01:05 +0100

    a

--- app/src/main/java/cat/itb/m08_uf1_p4/activities/MainActivity.java
+++ app/src/main/java/cat/itb/m08_uf1_p4/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package cat.itb.m08_uf1_p4;
+package cat.itb.m08_uf1_p4.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import cat.itb.m08_uf1_p4.R;
 
 public class MainActivity extends AppCompatActivity {
 
