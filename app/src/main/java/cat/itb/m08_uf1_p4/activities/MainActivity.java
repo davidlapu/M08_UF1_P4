b0c9f7cabb7d3ea3534d3c83f001432a2768e554
David Lama Pulido <david.lama.7e3@itb.cat>
2020-11-10 19:05:20 +0100

    a

--- app/src/main/java/cat/itb/m08_uf1_p4/activities/MainActivity.java
+++ app/src/main/java/cat/itb/m08_uf1_p4/activities/MainActivity.java
@@ -4,10 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.HashMap;
+
 import cat.itb.m08_uf1_p4.R;
+import cat.itb.m08_uf1_p4.models.Character;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private final Character[] characters = {new Character("Fishl", R.drawable.bow, R.drawable.element_Electro),
+            new Character("Diona", R.drawable.bow, R.drawable.element_Cryo),
+            new Character("Klee", R.drawable.catalyst, R.drawable.element_Pyro),
+            new Character("Keqing", R.drawable.sword, R.drawable.element_Electro),
+            new Character("Mona", R.drawable.catalyst, R.drawable.element_Hydro),
+            new Character("Ningguang", R.drawable.catalyst, R.drawable.element_Geo),
+            new Character("Noelle", R.drawable.catalyst, R.drawable.element_Geo),
+            new Character("Qiqi", R.drawable.sword, R.drawable.element_Cryo),
+            new Character("Venti", R.drawable.catalyst, R.drawable.element_Anemo),
+            new Character("Xiangling", R.drawable.polearm, R.drawable.element_Pyro)
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/cat/itb/m08_uf1_p4/models/Character.java
@@ -0,0 +1,12 @@
+package cat.itb.m08_uf1_p4.models;
+
+public class Character {
+    private String name;
+    private int waepon, element;
+
+    public Character(String name, int waepon, int element) {
+        this.name = name;
+        this.waepon = waepon;
+        this.element = element;
+    }
+}
